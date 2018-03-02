60867eed9883e54adec2f0d5ebfbac5dd326e375
Catalin Vlad <Catalin.Vlad@payu.ro>
2018-03-02 17:40:43 +0200

    ep 53 almost finished

--- project1/app.js
+++ project1/app.js
@@ -12,18 +12,51 @@ new Vue({
            this.monsterHealth = 100;
        },
         attack: function () {
-           var max = 10;
-           var min = 3;
-           var damage = Math.floor(Math.random() * max)
+            this.monsterHealth -= this.calculateDamage(3, 10);;
+            if (this.checkWin()) {
+                return;
+            }
+
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
         },
         specialAttack: function () {
-            
+            this.monsterHealth -= this.calculateDamage(3, 10);;
+            if (this.checkWin()) {
+                return;
+            }
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
         },
         heal: function () {
             
         },
         giveUp: function () {
 
+        },
+        monsterAttack: function () {
+
+        },
+        calculateDamage: function(min, max) {
+           return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function () {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! New game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You lost! New game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
         }
     }
 });
